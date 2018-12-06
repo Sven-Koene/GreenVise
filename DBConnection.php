cbd5d77c4ef0e1e2b54ce825869e422bce75a0fb
Sweetpurple616 <zoedewaard@hotmail.nl>
2018-12-06 14:50:00 +0100

    DBConnection aangemaakt

---
+++ DBConnection.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$database = "greenvise";
+$user = "root";
+$password  = "";
+
+$db = mysqli_connect($host, $user, $password, $database)
+    or die("Error: " . mysqli_connect_error());;
+    
